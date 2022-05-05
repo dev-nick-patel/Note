00585ef4c79d3537daf395d53716abfaea14d3ec
dev-nick-patel <nikunjpatel.au@gmail.com>
2022-05-05 16:07:53 +1000

    phase-1 few changes

--- app/src/main/java/com/techhand/note/MainActivity.kt
+++ app/src/main/java/com/techhand/note/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.techhand.note.ui.theme.NoteTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android Nikunj Jetpack Compose")
+                    Greeting("Android Jetpack Compose")
                 }
             }
         }
@@ -31,7 +33,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+        Text(
+            text = "Hello $name",
+            modifier = Modifier.padding(all = 4.dp),
+            style = MaterialTheme.typography.button
+        )
+    }
 }
 
 @Preview(showBackground = true)
