380b21a5e31d3a0864e48433696aa49a9cf37e6f
dev-nick-patel <nikunjpatel.au@gmail.com>
2022-06-26 20:07:05 +1000

    android yml changed

--- app/src/main/java/com/techhand/note/MainActivity.kt
+++ app/src/main/java/com/techhand/note/MainActivity.kt
@@ -3,15 +3,25 @@ package com.techhand.note
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.techhand.note.ui.theme.NoteTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,33 +29,61 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NoteTheme {
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android Jetpack Compose")
+                    Greeting()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun Greeting(name: String) {
-    Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
-        Text(
-            text = "Hello $name",
-            modifier = Modifier.padding(all = 4.dp),
-            style = MaterialTheme.typography.body2
-        )
+fun Greeting() {
+   var visible by remember {
+       mutableStateOf(true)
+   }
+    val density = LocalDensity.current
+
+    AnimatedVisibility(visible = visible,
+    enter = fadeIn(),
+    exit = fadeOut()) {
+
+        Box(modifier = Modifier.fillMaxSize()) {
+            Box(modifier = Modifier
+                .align(Alignment.Center)
+                .animateEnterExit(
+                    enter = slideInVertically(),
+                    exit = slideOutVertically()
+                )
+                .sizeIn(minWidth = 256.dp, minHeight = 64.dp)
+
+            )
+            Text(
+                text = stringResource(id = R.string.app_name).uppercase(),
+                modifier = Modifier
+                    .padding(all = 4.dp)
+                    .align(Alignment.Center),
+                style = MaterialTheme.typography.button,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
     }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     NoteTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
